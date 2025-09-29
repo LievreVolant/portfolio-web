670a11ec00f5ea00911687c4b57fbdbc76e8184e
LievreVolant <lievre.volant31@gmail.com>
2025-09-29 20:06:55 +0200

    base de projet

---
+++ src/app/components/closed-book/closed-book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-closed-book',
+  imports: [],
+  templateUrl: './closed-book.component.html',
+  styleUrl: './closed-book.component.scss'
+})
+export class ClosedBookComponent {
+
+}
---
+++ src/app/components/open-book/open-book.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-open-book',
+  imports: [],
+  templateUrl: './open-book.component.html',
+  styleUrl: './open-book.component.scss'
+})
+export class OpenBookComponent {
+
+}
