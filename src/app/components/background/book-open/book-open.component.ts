fbb0757b1dc27cc0ea4f579ce53cae3bab660eaf
Lili GOY <lili.goy@etu.univ-grenoble-alpes.fr>
2026-05-20 21:14:24 +0200

    Made one working tab

--- src/app/components/background/book-open/book-open.component.ts
+++ src/app/components/background/book-open/book-open.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
 import {BOOK_COVER_HEIGHT, BOOK_COVER_WIDTH, BOOK_PAGE_HEIGHT, BOOK_PAGE_WIDTH} from '../../../constants';
 import {RouterOutlet} from '@angular/router';
+import {TabComponent} from '../../shared/tab/tab.component';
 
 @Component({
   selector: 'app-book-open',
     imports: [
         RouterOutlet,
+        TabComponent,
     ],
   templateUrl: './book-open.component.html',
   styleUrl: './book-open.component.scss'
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,3 +2,13 @@ export const BOOK_COVER_WIDTH = 655;
 export const BOOK_COVER_HEIGHT = 800;
 export const BOOK_PAGE_WIDTH = BOOK_COVER_WIDTH - 50;
 export const BOOK_PAGE_HEIGHT = BOOK_COVER_HEIGHT - 60;
+
+export const COLOR_1 = '#0B1E38';
+export const COLOR_2 = '#173679';
+export const COLOR_3 = '#4888C8';
+export const COLOR_4 = '#7FC5DC';
+export const COLOR_5 = '#E8E163';
+export const COLOR_6 = '#DB901C';
+export const WHITE = '#FFFDFA';
+export const GREY = '#D9D9D9';
+export const BLACK = '#464646';
