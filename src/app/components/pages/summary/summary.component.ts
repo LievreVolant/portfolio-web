c24ca4f64873b1933187ede736d38a7a6e505c76
Lili GOY <lili.goy@etu.univ-grenoble-alpes.fr>
2026-05-24 23:03:59 +0200

    Tabs in progress

--- src/app/components/pages/summary/summary.component.ts
+++ src/app/components/pages/summary/summary.component.ts
@@ -1,12 +1,10 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {PageCornersComponent} from '../../shared/page-corners/page-corners.component';
-import {TabComponent} from '../../shared/tab/tab.component';
 
 @Component({
   selector: 'app-summary',
     imports: [
         PageCornersComponent,
-        TabComponent
     ],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
