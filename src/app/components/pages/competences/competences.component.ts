9e2744caaf691fd21954d2ccc3e5e26f5527aec9
Lili <lievrevolant31@gmail.com>
2026-06-20 17:07:07 +0200

    Added the comp languages competences

--- src/app/components/pages/competences/competences.component.ts
+++ src/app/components/pages/competences/competences.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {PageCornersComponent} from '../../shared/page-corners/page-corners.component';
+import { ProjectsService } from '../../../services/projects.service';
+import { LanguageData } from '../../../model/project.interface';
+import { IMG_SRC } from '../../../constants';
 
 @Component({
   selector: 'app-competences',
     imports: [
-        PageCornersComponent
+        PageCornersComponent,
     ],
   templateUrl: './competences.component.html',
-  styleUrl: './competences.component.scss'
+  styleUrl: './competences.component.scss',
 })
-export class CompetencesComponent {
+export class CompetencesComponent implements OnInit {
+    protected readonly imgSrc = IMG_SRC;
+    languages: string[] = ['Angular', 'C', 'C++', 'C#', 'Dart', 'Flutter', 'GraphQl', 'HTML / CSS', 'Java', 'JavaScript', 'Lua', 'OCaml', 'PHP', 'R', 'Python', 'SQL', 'Symfony', 'TypeScript', 'UML', 'XAML'];
+    projectLanguages: string[] = [];
+    favoriteLanguages: string[] = ['Angular', 'C#', 'HTML / CSS', 'TypeScript'];
 
+    constructor(protected projectsService: ProjectsService) { }
+    
+    ngOnInit() {
+        this.projectLanguages = this.projectsService.getLanguagesFromProjects();
+    }
+    
+    getLanguageData(language: string): LanguageData {
+        return this.projectsService.getLanguageData(language);
+    }
 }
--- src/app/components/pages/projects/projects.component.ts
+++ src/app/components/pages/projects/projects.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import {PageCornersComponent} from '../../shared/page-corners/page-corners.component';
 import {IMG_SRC} from '../../../constants';
-import recentProjectsData from '../../../data/recent-project-descriptions.json';
 import {RouterLink} from '@angular/router';
 
 @Component({
@@ -15,5 +14,4 @@ import {RouterLink} from '@angular/router';
 })
 export class ProjectsComponent {
     readonly imgSrc = IMG_SRC;
-    protected readonly recentProjectsData = recentProjectsData;
 }
