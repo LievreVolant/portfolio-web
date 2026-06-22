e4ed4f1f80613089f2be5f26d051d755707b133f
LievreVolant <156901075+LievreVolant@users.noreply.github.com>
2026-06-22 12:10:22 +0200

    Various ui fixes, mostly on career pages

--- src/app/components/pages/career/career.component.ts
+++ src/app/components/pages/career/career.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {PageCornersComponent} from '../../shared/page-corners/page-corners.component';
 import { LINKS } from '../../../constants';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-career',
     imports: [
         PageCornersComponent,
+        RouterLink,
     ],
   templateUrl: './career.component.html',
   styleUrl: './career.component.scss',
